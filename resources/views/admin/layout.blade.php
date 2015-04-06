52c81358180f68a0fbc355810511a81ff3d1a56e
Chaow Porkaew <pchaow@gmail.com>
2015-04-06 14:22:36 +0700

    add first layout

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -6,31 +6,68 @@
 
 <body>
 
-<div class="ui menu">
-    <div class="header item">
-        Success Model - ระบบฐานข้อมูลหนึ่งคณะหนึ่งโมเดล
-    </div>
-    <a class="item">
-        About Us
-    </a>
-    <a class="item">
-        Jobs
-    </a>
-    <a class="item">
-        Locations
-    </a>
-    <div class="right menu">
-        <div class="header item">
-            Support
+<div class="ui one column grid">
+    <div class="column">
+        <div class="ui menu">
+            <div class="header item">
+                Success Model - ระบบฐานข้อมูลหนึ่งคณะหนึ่งโมเดล
+            </div>
+            <a class="item active">
+                Main Menu
+            </a>
+            <a class="item">
+                About Us
+            </a>
+
+            <div class="right menu">
+                <div class="header item">
+                    Support
+                </div>
+                <a class="item">
+                    FAQ
+                </a>
+                <a class="item">
+                    E-mail Support
+                </a>
+            </div>
         </div>
-        <a class="item">
-            FAQ
-        </a>
-        <a class="item">
-            E-mail Support
-        </a>
+
     </div>
 </div>
+
+
+<div class="ui centered grid">
+    <div class="ui centered row">
+        <div class="ui three wide column">
+            <div class="ui secondary vertical menu">
+                <a class="active item">
+                    <i class="users icon"></i>
+                    Friends
+                </a>
+                <a class="item">
+                    <i class="mail icon"></i> Messages
+                </a>
+                <a class="item">
+                    <i class="user icon"></i> Friends
+                </a>
+                <div class="ui dropdown item">
+                    More
+                    <i class="dropdown icon"></i>
+                    <div class="menu">
+                        <a class="item"><i class="edit icon"></i> Edit Profile</a>
+                        <a class="item"><i class="globe icon"></i> Choose Language</a>
+                        <a class="item"><i class="settings icon"></i> Account Settings</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="ui twelve wide column">
+            XXX
+        </div>
+    </div>
+</div>
+
+
 
 </body>
 </html>
