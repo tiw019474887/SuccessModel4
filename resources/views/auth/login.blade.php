e7f9b5a4bba9cbb929eace4ec8948052b6d98c15
Chaow Porkaew <pchaow@gmail.com>
2015-04-18 14:55:11 +0700

    add login panel

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,17 +5,15 @@
     <link rel="stylesheet" href="/packages/semantic-ui/dist/components/dropdown.min.css"/>
 
 
-
-
     <script src="/packages/jquery/dist/jquery.min.js"></script>
     <script src="/packages/semantic-ui/dist/semantic.min.js" type="text/javascript"></script>
     <script src="/packages/semantic-ui/dist/components/dropdown.min.js" type="text/javascript"></script>
     <style>
         .avatar-menu {
             height: 2em !important;
-            width : 2em !important;
-            margin-top : -0.5em;
-            margin-bottom:  -0.5em;
+            width: 2em !important;
+            margin-top: -0.5em;
+            margin-bottom: -0.5em;
         }
     </style>
 </head>
@@ -26,6 +24,7 @@
 
     <h2 class="ui header inverted" style="padding: 10px;">
         <img src="/images/uplogo.png">
+
         <div class="content">
             Success Model
             <div class="sub header">ระบบฐานข้อมูลโครงการหนึ่งคณะหนึ่งโมเดล</div>
@@ -35,15 +34,33 @@
 </div>
 
 <div class="ui padded  grid ">
-        <div class="twelve wide column">
-            asdf
+    <div class="twelve wide column">
+        <div class="ui purple segment">
+            Purple
         </div>
-        <div class="four wide column">
-            asdf
+    </div>
+    <div class="four wide column">
+        <div class="ui top attached purple inverted  segment">
+            <h4>เข้าใช้งานระบบ / Sign in</h4>
+        </div>
+        <div class="ui attached segment">
+            <form class="ui form">
+                <div class="ui fluid left icon input field">
+                    <i class="mail icon"></i>
+                    <input type="text" placeholder="E-Mail Address">
+                </div>
+                <div class="ui fluid left icon input field">
+                    <i class="lock icon"></i>
+                    <input type="password" placeholder="E-Mail Address">
+                </div>
+
+                <div class="fluid ui primary button">User Login</div>
+
+            </form>
         </div>
     </div>
 </div>
-
+</div>
 
 
 <script type="text/javascript">
