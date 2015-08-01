d76e26ff1040fd1f72ceda7f8278e043f8853fb0
namningse <vaan_ratchunarak@hotmail.com>
2015-08-01 18:52:30 +0700

    สร้าง Sugestion ตอนอนุมัติ

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -42,10 +42,16 @@ Route::group([
 
 
 Route::group(['prefix' => 'api'], function () {
+
+    //api for suggestion
+    Route::get('suggestion/get-suggestion','API\ProjectSuggestionApiController@getSuggestions');
+    Route::post('suggestion/add-projectsuggestion','API\ProjectSuggestionApiController@addProjectSuggestion');
+
+
     //api for comment
     Route::get('comment/get-comment','API\ProjectCommentApiController@getComments');
-    Route::post('comment/add-projectcomment','API\ProjectCommentApiController@addProjectComments');
-    Route::post('comment/add-commentcomment','API\ProjectCommentApiController@addCommentComments');
+    Route::post('comment/add-projectcomment','API\ProjectCommentApiController@addProjectComment');
+    Route::post('comment/add-commentcomment','API\ProjectCommentApiController@addCommentComment');
 
     //api for faculty
     Route::get('faculty/projects','API\FacultyProjectApiController@getProjects');
---
+++ app/Models/Suggestion.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Models;
+
+/**
+ * Created by PhpStorm.
+ * UserRequest: chaow
+ * Date: 4/6/2015
+ * Time: 12:59 PM
+ */
+
+use Vinelab\NeoEloquent\Eloquent\SoftDeletes;
+
+class Suggestion extends \NeoEloquent
+{
+
+    use SoftDeletes;
+
+    protected $label = ['Suggestion'];
+
+    protected $fillable = ['suggestion'];
+
+
+    public function createdBy(){
+        return $this->hasOne("App\Models\User","CREATE_BY");
+    }
+    public function suggestions(){
+        return $this->hasMany("App\Models\Suggestion","HAS_SUGGESTION");
+    }
+
+}
