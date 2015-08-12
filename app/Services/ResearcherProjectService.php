8e8c8fdcfa1e04f55d2c8f1d8efc257b0e68679b
namningse <vaan_ratchunarak@hotmail.com>
2015-08-13 00:56:02 +0700

    แก้ไข ResearcherService

--- app/Services/ResearcherProjectService.php
+++ app/Services/ResearcherProjectService.php
@@ -90,7 +90,7 @@ class ResearcherProjectService extends ProjectService
         $project = Project::find($id);
         if($project){
             /* @var Project $project*/
-            return $project->status;
+            //return $project->status;
             if($project->status->key =='draft'){
                 $this->linkToFacultyStatus($project,$input);
             }else{
