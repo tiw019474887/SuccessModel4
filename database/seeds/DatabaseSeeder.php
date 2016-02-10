e83c87ea3df792d404b9b499dcd9493df0bf33ea
namningse <vaan_ratchunarak@hotmail.com>
2016-02-10 16:10:51 +0700

    up

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder {
         $this->call("AreaSeeder");
         $this->call("FacultySeeder");
         $this->call("ProjectStatusSeeder");
+        $this->call("ProjectSeeder");
 
 
 
