574b0cab7e17ecbf0c2aeabc94107fff39ccd58a
tiw019474887 <tiw_t1@hotmail.com>
2016-05-16 01:08:14 +0700

    เปลี่ยนหน้ารีวิว และ ตำแหน่งช่อง ค้นหา

--- resources/views/admin/project/main.blade.php
+++ resources/views/admin/project/main.blade.php
@@ -38,12 +38,55 @@
 
 
 @section('javascript')
+    <script>
+        var app = angular.module('demoapp', ['Project', 'Area', 'openlayers-directive']);
+        app.controller('DemoController', ['$scope', function ($scope) {
+            angular.extend($scope, {});
+
+        }]);
+
+    </script>
+
+    <script type="text/javascript">
+
+        $('#logIn').click(function () {
+            $('#modaldiv').modal('show');
+        });
+
+    </script>
+
+    <script type="text/javascript">
+        $(window).load(function () {
+            $('.flexslider').flexslider({
+                animation: "slide"
+            });
+        });
+    </script>
+
+
+
+
+    <script src="/app/admin/loader.js"></script>
+    <script src="/app/users/project/app.js"></script>
+
+    <script type="text/javascript" src="/packages/openlayers/build/ol.js"></script>
+    <script type="text/javascript" src="/packages/angular/angular.min.js"></script>
+    <script type="text/javascript"
+            src="/packages/angular-sanitize/angular-sanitize.min.js"></script>
+    <script type="text/javascript"
+            src="/packages/angular-openlayers-directive/dist/angular-openlayers-directive.js"></script>
+    <link rel="stylesheet" href="/packages/openlayers/build/ol.css"/>
+
+
     <script type="text/javascript" src="/packages/flexslider/jquery.flexslider-min.js"></script>
     <script type="text/javascript" src="/packages/angular-flexslider/angular-flexslider.js"></script>
     <script type="text/javascript" src="/packages/showdown/compressed/Showdown.js"></script>
     <script type="text/javascript" src="/packages/angular-sanitize/angular-sanitize.min.js"></script>
     <script type="text/javascript" src="/packages/angular-markdown-directive/markdown.js"></script>
     <script type="text/javascript" src="/packages/bxslider/jquery.bxSlider.min.js"></script>
+    <script type="text/javascript" src="/app/researcher/ResearcherService.js"></script>
+    <script type="text/javascript" src="/app/users/UsersService.js"></script>
+    <script type="text/javascript" src="/app/admin/AreaService.js"></script>
     <script type="text/javascript" src="/app/admin/YoutubeService.js"></script>
     <script type="text/javascript" src="/app/admin/UserService.js"></script>
     <script type="text/javascript" src="/app/admin/AreaService.js"></script>
