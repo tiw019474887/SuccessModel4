a5a74d1b329347cca6eb594b2f833cda81bba419
namningse <vaan_ratchunarak@hotmail.com>
2016-06-14 16:19:50 +0700

    up

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -1,13 +1,6 @@
 <?php
 namespace App\Models;
 
-/**
- * Created by PhpStorm.
- * UserRequest: chaow
- * Date: 4/6/2015
- * Time: 12:59 PM
- */
-
 use Vinelab\NeoEloquent\Eloquent\SoftDeletes;
 
 class Video extends \NeoEloquent
