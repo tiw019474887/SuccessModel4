6d96e5e3b89001e2aa97dd4a5b0475d38e4994da
namningse <vaan_ratchunarak@hotmail.com>
2016-07-17 17:50:14 +0700

    คำผิด

--- resources/views/users/project/mainyear/mainyear2555.blade.php
+++ resources/views/users/project/mainyear/mainyear2555.blade.php
@@ -36,10 +36,10 @@
         <div class="ui attached stackable menu">
             <div class="ui container">
                 <a class="active item" href="/users">
-                    โครการทั้งหมด
+                    โครงการทั้งหมด
                 </a>
                 <div class="ui simple dropdown item">
-                    โครการในแต่ละอำเภอ
+                    โครงการในแต่ละอำเภอ
                     <i class="dropdown icon"></i>
                     <div class="menu">
                         <div class="item"><a href="/users/district/maindistrictMaung"> อำเภอเมือง</a></div>
@@ -54,7 +54,7 @@
                     </div>
                 </div>
                 <div class="ui simple dropdown item">
-                    โครการในแต่ละคณะ
+                    โครงการในแต่ละคณะ
                     <i class="dropdown icon"></i>
                     <div class="menu">
                         <div class="item"><a href="/users/faculty/Ag">คณะเกษตรศาสตร์และทรัพยากรธรรมชาติ</a></div>
@@ -76,7 +76,7 @@
                     </div>
                 </div>
                 <div class="ui simple dropdown item">
-                    โครการในแต่ละปี
+                    โครงการในแต่ละปี
                     <i class="dropdown icon"></i>
                     <div class="menu">
                         <div class="item"><a href="/users/year/mainYear2555"> 2555</a></div>
